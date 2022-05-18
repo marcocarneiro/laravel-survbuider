b1f30ef20596191ae3493035af7c3866d71d82d5
marcocarneiro <marco.carneiro7@gmail.com>
2022-05-18 12:50:31 -0300

    Update 2022_05_16_140948_create_resultados_table.php

--- database/migrations/2022_05_16_140948_create_resultados_table.php
+++ database/migrations/2022_05_16_140948_create_resultados_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('resultados', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('id_pesquisa');
+            $table->foreign('id_pesquisa')->references('id')->on('pesquisas');
             $table->timestamps();
         });
     }
@@ -29,3 +31,12 @@ return new class extends Migration
         Schema::dropIfExists('resultados');
     }
 };
+
+/* 
+<b>aceite</b> - Aceite do participante após a leitura da página inicial da pesquisa <br>
+<b>ip_usuario</b> - IP do usuário no momento em que inicia a pesquisa <br>
+<b>navegador</b> - Navegador do usuário no momento em que inicia a pesquisa <br>
+<b>data_hora_inicio</b> - Data e hora quando usuário iniciou a pesquisa <br>
+<b>data_hora_final</b> - Data e hora quando usuário finalizou a pesquisa <br>
+<b>dados</b> - Respostas do usuário no formato JSON <br>
+<b>completo</b> - Booleano, indica se o usuário finalizou a pesquisa ou não <br> */
