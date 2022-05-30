97114adbc7a0a5774c782e6089b691d5394a7dff
marcocarneiro <marco.carneiro7@gmail.com>
2022-05-30 17:31:32 -0300

    Implementação de componente

---
+++ app/Http/Livewire/Pesquisas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Pesquisas extends Component
+{
+    public function render()
+    {
+        return view('livewire.pesquisas');
+    }
+}
---
+++ resources/views/livewire/pesquisas.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.base')
+
+@section('title', 'DASHBOARD')
+
+@section('conteudo')
+
+<div class="md:grid grid-cols-3 gap-4 mb-10">
+
+    <p>Tela para criar as pesquisas</p>
+
+</div>    
+
+@endsection
