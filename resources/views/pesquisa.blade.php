63b81bda22581bd919fbde1c651e814dcbc769fc
marcocarneiro <marco.carneiro7@gmail.com>
2022-07-03 21:51:25 -0300

    Update pesquisa.blade.php

--- resources/views/pesquisa.blade.php
+++ resources/views/pesquisa.blade.php
@@ -56,15 +56,17 @@
                     @endif
                 @endfor
             @endforeach
+
+            <div class="flex justify-center mt-6">
+                <button onClick="avancar()" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium 
+                rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    Prosseguir
+                </button>
+            </div>
         </div>
     @endforeach
 
-    <div class="flex justify-center">
-        <button onClick="avancar()" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium 
-        rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-            Prosseguir
-        </button>
-    </div>
+    
         
 </div>
 
