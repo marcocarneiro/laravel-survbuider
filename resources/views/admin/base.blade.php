e41ffa5563b897401ce7f668f135879b77785318
marcocarneiro <marco.carneiro7@gmail.com>
2022-07-27 23:53:03 -0300

    update 27/07 23:52

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -59,13 +59,16 @@
                   </div>
                   <ul class="py-1" aria-labelledby="dropdown">
                     <li>
-                      <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                      <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600
+                       dark:text-gray-200 dark:hover:text-white">Dashboard</a>
                     </li>
                     <li>
-                      <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                      <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100
+                       dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
                     </li>
                     <li>
-                      <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                      <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600
+                       dark:text-gray-200 dark:hover:text-white">Earnings</a>
                     </li>
                     <li>
                         <form method="POST" action="{{ route('logout') }}">
@@ -93,19 +96,22 @@
             <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
               <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                 <li>
-                  <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                  <a href="{{ route('dashboard') }}" class="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-200 hover:bg-gray-50 
+                  md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-gray-400
+                   md:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent
+                    dark:border-gray-400">Painel</a>
                 </li>
                 <li>
-                  <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                  <a href="{{ route('new-pesquisa') }}" class="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-200 hover:bg-gray-50 
+                  md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-gray-400
+                   md:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent
+                    dark:border-gray-400">Criar pesquisa</a>
                 </li>
                 <li>
-                  <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                </li>
-                <li>
-                  <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
-                </li>
-                <li>
-                  <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                  <a href="#" class="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-200 hover:bg-gray-50 
+                  md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-gray-400
+                   md:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent
+                    dark:border-gray-400">Resultados</a>
                 </li>
               </ul>
             </div>
