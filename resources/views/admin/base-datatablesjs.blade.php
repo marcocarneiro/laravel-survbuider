4059fd663bbc8a5f53f2f6c9fcae2414c872f0a6
marcocarneiro <marco.carneiro7@gmail.com>
2022-07-29 11:07:27 -0300

    Implementação resultados pesquisa

---
+++ resources/views/admin/base-datatablesjs.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>SurvBuilder | @yield('title')</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ url('/') }}/css/base.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+
+        @livewireStyles
+        
+
+        <!-- Scripts -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js" integrity="sha256-xKeoJ50pzbUGkpQxDYHD7o7hxe0LaOGeguUidbq6vis=" crossorigin="anonymous"></script>
+        <script src="{{ mix('js/app.js') }}" defer></script>
+
+        <!-- TinyMCE  -->
+        <script src="{{ asset('js/tinymce/tinymce.min.js') }}" referrerpolicy="origin"></script>        
+
+    </head>
+    <body class="max-h-screen font-sans antialiased bg-slate-300 text-slate-700">
+
+        {{-- Navbar --}}
+        <nav class="bg-slate-500 fixed text-slate-200 px-2 sm:px-4 py-2.5 w-full drop-shadow-md z-50">
+            <div class="container flex flex-wrap justify-between items-center mx-auto">
+            <a href="#" class="flex items-center">
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">SurvBuilder</span>
+            </a>
+            
+            <!-- user menu -->
+            <div class="flex items-center md:order-2">
+                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                    <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" type="button" data-dropdown-toggle="dropdown">
+                        <span class="sr-only">Open user menu</span>
+                        <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                    </button>
+                @else
+                    <span class="inline-flex rounded-md">
+                        <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition" id="user-menu-button" aria-expanded="false" type="button" data-dropdown-toggle="dropdown">
+                            {{ Auth::user()->name }}
+
+                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                    </span>
+                @endif
+                <div class="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown">
+                  <div class="py-3 px-4">
+                    <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+                    <span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                  </div>
+                  <ul class="py-1" aria-labelledby="dropdown">
+                    <li>
+                      <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600
+                       dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                    </li>
+                    <li>
+                      <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100
+                       dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                    </li>
+                    <li>
+                      <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600
+                       dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                    </li>
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <a class="nav-link text-light" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                        this.closest('form').submit(); " role="button">
+                                <i class="fas fa-sign-out-alt"></i>
+                    
+                                {{ __('Log Out') }}
+                            </a>
+                        </form>
+                    </li>
+                  </ul>
+                </div>                
+
+                {{-- menu hamburguer --}}
+                <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-900 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+              </button>
+            </div>
+
+            {{-- menu principal --}}
+            <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
+              <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                <li>
+                  <a href="{{ route('dashboard') }}" class="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-200 hover:bg-gray-50 
+                  md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-gray-400
+                   md:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent
+                    dark:border-gray-400">Painel</a>
+                </li>
+                <li>
+                  <a href="{{ route('new-pesquisa') }}" class="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-200 hover:bg-gray-50 
+                  md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-gray-400
+                   md:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent
+                    dark:border-gray-400">Criar pesquisa</a>
+                </li>
+                <li>
+                  <a href="#" class="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-200 hover:bg-gray-50 
+                  md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-gray-400
+                   md:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white md:dark:hover:bg-transparent
+                    dark:border-gray-400">Resultados</a>
+                </li>
+              </ul>
+            </div>
+            </div>
+        </nav>        
+
+        <div class="w-full h-full p-10 pt-24">
+            @yield('conteudo')
+        </div>        
+
+        @livewireScripts
+        <script src="https://unpkg.com/flowbite@1.4.6/dist/flowbite.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+        <script>
+
+        </script>              
+    </body>
+</html>
--- resources/views/admin/resultado.blade.php
+++ resources/views/admin/resultado.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.base')
+@extends('admin.base-datatablesjs')
 
 @section('title', 'RESULTADOS')
 
@@ -6,14 +6,38 @@
 
 <h5 class="mb-2 text-2xl text-center m-5 font-bold tracking-tight text-gray-600 dark:text-white">{{$pesquisa->titulo}}</h5>
 <div class="flex justify-center">    
-    <div class="w-5/6 divide-y mb-10 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        @foreach ($resultados as $resultado)        
-            <div class="p-6">                
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 
-                    so far, in reverse chronological order.
-                </p>
-            </div>        
-        @endforeach
+    <div class="w-5/6 p-5 mb-10 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <table>
+            <thead>
+                <tr>
+                    <th>IP user</th>    
+                    <th>Início</th>
+                    <th>Final</th>
+                    <th>Dados</th>
+                    <th>Completo</th>
+                </tr>
+            </thead>
+            <tbody> 
+            @foreach ($resultados as $resultado)
+                <tr>
+                    <td>{{$resultado->ip_usuario}}</td>
+                    <td>{{$resultado->data_hora_inicio}}</td>
+                    <td>{{$resultado->data_hora_final}}</td>
+                    <td>{{$resultado->dados}}</td>
+                    <td>{{$resultado->completo}}</td>             
+                </tr>                    
+            @endforeach
+            </tbody>
+            <tfoot>
+            <tr>
+                <th>IP user</th>    
+                <th>Início</th>
+                <th>Final</th>
+                <th>Dados</th>
+                <th>Completo</th>
+            </tr>
+        </tfoot>
+        </table>
     </div>
 </div>    
 
